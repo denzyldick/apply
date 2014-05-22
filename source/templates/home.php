62af0c3d9830b31abcc10968bb71133370546f8a
denzyl <denzyl@live.nl>
2014-05-22 23:10:01 +0200

    Added first template

---
+++ source/templates/home.php
@@ -0,0 +1,72 @@
+<html>
+	<head>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<title>Appply</title>
+		</head>
+		<style>
+			@font-face
+{
+font-family: flex;
+src: url(fonts/light.otf);
+}
+.brand{
+	font-family: flex;
+	}
+body { 
+  background: url(http://cssauthor.com/wp-content/uploads/2013/02/4_1.png) no-repeat center center fixed; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}.jumbotron{
+	background-color:#fff!important;}
+	.copright{
+		color: #ffffff!important;
+		}
+</style>
+<body>
+	
+	<div class="navbar navbar-inverse">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-inverse-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#"><span style="font-size: 24px" class='brand'>Appply</span></a>
+  </div>
+  <div class="navbar-collapse collapse navbar-inverse-collapse">
+    <ul class="nav navbar-nav">
+
+      
+    </ul>
+
+    <ul class="nav navbar-nav navbar-right">
+    
+      <li class="dropdown">
+       <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Account </a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+
+<div class="jumbotron " style="max-width:400px;margin: 0 auto;margin-top:45px;text-align:center;border-radius: 5px;
+border: 1px solid #b81e0d">
+  <h1 class='brand'>Appply</h1>
+  <p>lorum upsom</p>
+  <p><a class="btn btn-primary btn-lg" role="button">Sign up</a>&nbsp;<a class="btn btn-primary btn-lg" role="button">Login</a></p>
+</div>
+<div class=copright style="max-width:400px;margin: 0 auto;text-align:center;margin-top: 10px">&copy; 2014 Appply. All rights reserved.</div>
+</body>
+</html>
