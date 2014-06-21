dd5e210cf64c6bad473095152894d37e40b9426f
denzyl <denzyl@live.nl>
2014-06-21 23:27:46 +0200

    Added company model

---
+++ app/models/Company.php
@@ -0,0 +1,57 @@
+<?php
+
+class Company extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     *
+     * @var string
+     */
+    public $longitude;
+
+    /**
+     *
+     * @var string
+     */
+    public $latitude;
+
+    /**
+     *
+     * @var integer
+     */
+    public $user_id;
+
+    /**
+     * Independent Column Mapping.
+     */
+    public function columnMap()
+    {
+        return array(
+            'id' => 'id', 
+            'name' => 'name', 
+            'description' => 'description', 
+            'longitude' => 'longitude', 
+            'latitude' => 'latitude', 
+            'user_id' => 'user_id'
+        );
+    }
+
+}
