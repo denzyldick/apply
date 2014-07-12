0808e52218faa4e8f7101f5719477112802f7cef
Denzyl Dick <denzyl@live.nl>
2014-07-13 01:19:12 +0200

    Getters and Setters

--- app/models/Skills.php
+++ app/models/Skills.php
@@ -7,17 +7,59 @@ class Skills extends \Phalcon\Mvc\Model
      *
      * @var integer
      */
-    public $id;
+    protected $id;
 
     /**
      *
      * @var string
      */
-    public $name;
+    protected $name;
 
     /**
-     * Independent Column Mapping.
+     * Method to set the value of field id
+     *
+     * @param integer $id
+     * @return $this
      */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field name
+     *
+     * @param string $name
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the value of field name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
     public function columnMap()
     {
         return array(
