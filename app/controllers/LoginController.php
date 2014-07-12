bfbe6f92262524108b868e2c10eb28d80882febb
Denzyl Dick <denzyl@live.nl>
2014-07-13 01:29:06 +0200

    Removed unused codes

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -3,20 +3,19 @@
 class LoginController extends ControllerBase {
 
     public function indexAction() {
-        parent::initialize();
+    
     }
 
-  
+
     public function loginAction() {
-      
+
         if ($this->request->isPost()) {
             $email = $this->request->getPost("email");
             $password = $this->request->getPost("password");
             $remember = $this->request->getPost("remember");
 
             if ($this->check($email, $password, $remember)) {
-            
-              echo "Hello world";
+
             } else {
                 echo json_encode(array("status" => "false", "message" => "Wrong email or password"));
             }
