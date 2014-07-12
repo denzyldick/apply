a930aebd475a670820ba2b87a524d8562d569461
Denzyl Dick <denzyl@live.nl>
2014-07-13 01:19:44 +0200

    Getters and Setters

--- app/models/Specification.php
+++ app/models/Specification.php
@@ -7,29 +7,117 @@ class Specification extends \Phalcon\Mvc\Model
      *
      * @var integer
      */
-    public $id;
+    protected $id;
 
     /**
      *
      * @var string
      */
-    public $percent;
+    protected $percent;
 
     /**
      *
      * @var integer
      */
-    public $vacancy_id;
+    protected $vacancy_id;
 
     /**
      *
      * @var integer
      */
-    public $skills_id;
+    protected $skills_id;
 
     /**
-     * Independent Column Mapping.
+     * Method to set the value of field id
+     *
+     * @param integer $id
+     * @return $this
      */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field percent
+     *
+     * @param string $percent
+     * @return $this
+     */
+    public function setPercent($percent)
+    {
+        $this->percent = $percent;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field vacancy_id
+     *
+     * @param integer $vacancy_id
+     * @return $this
+     */
+    public function setVacancyId($vacancy_id)
+    {
+        $this->vacancy_id = $vacancy_id;
+
+        return $this;
+    }
+
+    /**
+     * Method to set the value of field skills_id
+     *
+     * @param integer $skills_id
+     * @return $this
+     */
+    public function setSkillsId($skills_id)
+    {
+        $this->skills_id = $skills_id;
+
+        return $this;
+    }
+
+    /**
+     * Returns the value of field id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Returns the value of field percent
+     *
+     * @return string
+     */
+    public function getPercent()
+    {
+        return $this->percent;
+    }
+
+    /**
+     * Returns the value of field vacancy_id
+     *
+     * @return integer
+     */
+    public function getVacancyId()
+    {
+        return $this->vacancy_id;
+    }
+
+    /**
+     * Returns the value of field skills_id
+     *
+     * @return integer
+     */
+    public function getSkillsId()
+    {
+        return $this->skills_id;
+    }
     public function columnMap()
     {
         return array(
@@ -39,10 +127,6 @@ class Specification extends \Phalcon\Mvc\Model
             'skills_id' => 'skills_id'
         );
     }
-
-    public functioin initialize()
-    {
-      $this->hasMany("skills_id","Skills","id");
-    }
+  
 
 }
