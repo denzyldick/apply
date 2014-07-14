6eab51b3b4c42be61d3f3e4ed85b5209aff3df8c
Denzyl Dick <denzyl@live.nl>
2014-07-14 21:14:44 +0200

    Removed validation and ajax implementation

--- app/controllers/SignupController.php
+++ app/controllers/SignupController.php
@@ -1,93 +1,30 @@
 <?php
-
 class SignupController extends ControllerBase {
-
-public function indexAction() {
-
-}
+    public function indexAction() {
+    }
     public function startAction() {
-         $this->view->disable();
         if ($this->request->isPost() == true) {
-            $user = new User();
-
             $email = $this->request->getPost('email');
             $firstname = $this->request->getPost('firstname');
             $lastname = $this->request->getPost('lastname');
             $password = $this->request->getPost('password');
             $re_type = $this->request->getPost('re_type');
             $type = $this->request->getPost('type');
-
-            if ($type == "employer") {
-                $type = "Employer";
-            } else {
-                $type = "Employee";
+            $user = new User();
+            $user->setEmail($email);
+            $user->setFirstname($firstname);
+            $user->setLastname($lastname);
+            $user->setPassword($this->security->hash($password));
+            $user->setType((empty($type) == true ? "employee" : "employer"));
+            if ($user->save()) {
+                if ($user->getType() == "employer") {
+                    $company = new Company();
+                    $company->user_id = $user->getId();
+                    $company->save();
+                }
+                $this->flash->success("Your account has been created.");
+                $this->dispatcher->forward(array("controller" => "login", "action" => "index"));
             }
-
-            $errormessage = "Please: <br/>";
-            $status = true;
-
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-
-                $errormessage .= "enter a valid email adres<br/>";
-                $status = false;
-            }
-            if (empty($firstname)) {
-
-                $errormessage .= "enter your firstname.";
-                $status = false;
-            }
-            if (empty($lastname)) {
-
-                $errormessage .= "enter your lastname";
-                $status = false;
-            }
-            if (empty($password)) {
-                $errormessage .= "enter your password";
-                $status = false;
-            }
-
-            if ($re_type != $password) {
-                $errormessage .= "password doesn't match";
-                $status = false;
-            }
-
-            if ($status) {
-
-                $user->setEmail($email);
-                $user->setFirstname($firstname);
-                $user->setLastname($lastname);
-                $user->setPassword($this->security->hash($password));
-                $user->setType($type);
-
-        if($user->getType() == "employer")
-                      {
-                        $company = new Company();
-                        $company->user_id = $user->getId();
-                        $company->save();
-                      }
-               if( $user->save())
-               {
-                 $this->flash->success("Your account has been created.");
-                 return $this->dispatcher->forward(array("controller"=>"login","action"=>"index"));
-
-               }else
-               {
-                   foreach ($user->getMessages() as $message) {
-    echo $message;
-  }   }
-
-
-            } else {
-                echo(json_encode(array("status" => "false", "message" => $errormessage)));
-            }
-        } else {
-
-
-            $response = new \Phalcon\Http\Response();
-            $response->setStatusCode(404, "Not Found");
-            $response->setContent("<h1>404 :( </h1>Sorry, the page doesn't exist");
-            $response->send();
         }
     }
-
 }
