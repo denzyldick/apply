dfcf9263f447e482266fe0f4064d2f061d176cd6
Denzyl Dick <denzyl@live.nl>
2014-07-14 21:14:09 +0200

    Dispatcher to matches controller

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -1,25 +1,19 @@
 <?php
-
 class LoginController extends ControllerBase {
-
     public function indexAction() {
-
     }
-
-
     public function loginAction() {
-
         if ($this->request->isPost()) {
             $email = $this->request->getPost("email");
             $password = $this->request->getPost("password");
             $remember = $this->request->getPost("remember");
-
             if ($this->check($email, $password, $remember)) {
-              return  $this->dispatcher->forward(array("controller"=>"matches","action"=>"index"));
+                $this->dispatcher->forward(array("controller" => "matches", "action" => "index"));
             } else {
                 echo json_encode(array("status" => "false", "message" => "Wrong email or password"));
+                $this->flash->error("Wrong password or email");
+                $this->dispatcher->forward(array("action" => "index"));
             }
         }
     }
-
 }
